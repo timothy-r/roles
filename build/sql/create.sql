f45f264bf918e467e94d6ed6f6bafa406c84c908
Tim Rodger <tim.rodger@gmail.com>
2017-07-19 14:50:47 +0100

    Adds storage of role members

--- build/sql/create.sql
+++ build/sql/create.sql
@@ -1,9 +1,25 @@
 CREATE database role_data;
 
--- roles table just contains the name initially
 
 CREATE TABLE roles (
+  id SERIAL PRIMARY KEY ,
+  name varchar(512) NOT NULL UNIQUE,
+  description varchar(2048) 
 
+);
+
+CREATE TABLE members (
+  id SERIAL PRIMARY KEY ,
   name varchar(512) NOT NULL UNIQUE
+);
 
+-- bond table between roles and members, allowing a many to many relationship
+CREATE TABLE roles_members (
+  role_id INTEGER NOT NULL,
+  member_id INTEGER NOT NULL,
+  PRIMARY KEY (role_id, member_id),
+  CONSTRAINT rm_role_id_fkey FOREIGN KEY (role_id)
+    REFERENCES roles (id),
+  CONSTRAINT rm_member_id_fkey FOREIGN KEY (member_id)
+    REFERENCES members (id)
 );
